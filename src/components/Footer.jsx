149e6bef29b73f30c2d1ee9146b33ef54f021420
Berkay Urun <berkayurun@outlook.com>
2023-01-03 15:41:17 +0300

    update footer to align icon and increase icon size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <footer className=" bg-sky-800 py-6 text-center text-lg text-sky-50 transition-all duration-500 ease-out hover:bg-sky-500">
       <p>
-        Made with <AiFillHeart fill="red" className="mx-auto inline" /> (and
-        occasional tears) by{" "}
+        Made with{" "}
+        <AiFillHeart fill="red" className="mx-auto mb-1 inline" size={24} />{" "}
+        (and occasional tears) by{" "}
         <a
           href="https://github.com/brkyurun"
           className="hover:text-cyan-200 hover:underline hover:underline-offset-2"
