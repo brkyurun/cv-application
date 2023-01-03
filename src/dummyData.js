79ea6237ce5b3c0b4cec45da2f556dbf05ef8feb
Berkay Urun <berkayurun@outlook.com>
2023-01-03 15:20:15 +0300

    create dummy data to easily fill every field

---
+++ src/dummyData.js
@@ -0,0 +1,55 @@
+const aboutSectionDummy = {
+  firstName: "Mark",
+  lastName: "Johnson",
+  jobTitle: "Senior Vim Addict",
+  address: "London, UK",
+  phone: "+1 987-654-32-10",
+  mailAddress: "markjohnson@example.com",
+  about:
+    "Hello! I'm Mark Johnson, a Frontend Developer from London, working mainly with React, TailwindCSS, and SCSS. I value a web application's aesthetic presentation and enjoy crafting beautiful yet functional interfaces for users. Developing a UI that has the end-user amazed is what motivates me to further improve myself and the product!",
+};
+
+const educationSectionDummy = [
+  {
+    schoolName: "Cambridge University",
+    schoolLocation: "London, UK",
+    schoolDegree: "BSc",
+    schoolSubject: "Computer Science",
+    schoolStart: "2017",
+    schoolGraduate: "2021",
+  },
+  {
+    schoolName: "Some Highschool",
+    schoolLocation: "London, UK",
+    schoolDegree: "Honours",
+    schoolSubject: "High School",
+    schoolStart: "2013",
+    schoolGraduate: "2017",
+  },
+];
+
+const experienceSectionDummy = [
+  {
+    jobTitle: "Junior Copy-Paste Enthusiast",
+    companyName: "Googlesoftflix",
+    companyLocation: "London",
+    startDate: "12/2022",
+    endDate: "Current",
+  },
+  {
+    jobTitle: "Intern Copy-Paste Enthusiast",
+    companyName: "Googlesoftflix",
+    companyLocation: "London",
+    startDate: "06/2022",
+    endDate: "11/2022",
+  },
+  {
+    jobTitle: "Barista",
+    companyName: "StarDolares",
+    companyLocation: "London",
+    startDate: "02/2021",
+    endDate: "11/2021",
+  },
+];
+
+export { aboutSectionDummy, educationSectionDummy, experienceSectionDummy };
